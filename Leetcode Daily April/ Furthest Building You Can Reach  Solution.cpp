0b8fff740d3af64b4dc2e23234220bf583368e76
Dikshit Gupta <dgupta@mt.iitr.ac.in>
2021-04-27 11:20:01 +0530

    Leetcode 27 April priority queue

---
+++ Leetcode Daily April/ Furthest Building You Can Reach  Solution.cpp
@@ -0,0 +1,33 @@
+// You are given an integer array heights representing the heights of buildings, some bricks, and some ladders.
+
+// You start your journey from building 0 and move to the next building by possibly using bricks or ladders.
+
+// While moving from building i to building i+1 (0-indexed),
+
+// If the current building's height is greater than or equal to the next building's height, you do not need a ladder or bricks.
+// If the current building's height is less than the next building's height, you can either use one ladder or (h[i+1] - h[i]) bricks.
+// Return the furthest building index (0-indexed) you can reach if you use the given ladders and bricks optimally.
+
+class Solution {
+public:
+    int furthestBuilding(vector<int>& h, int b, int l) {
+        priority_queue<int> pq;
+        int br=0;
+        int temp;
+        
+        for(int i=0;i<h.size()-1;i++){    
+            temp=h[i+1]-h[i];
+            if(temp>0) {
+                pq.push(-1*temp);    
+                if(pq.size()>l){
+                    temp=-1*pq.top();
+                    pq.pop();
+                    br+=temp;
+                    if(br>b) return i;
+                }    
+            }
+        }
+        return h.size()-1;
+    
+    }
+};
