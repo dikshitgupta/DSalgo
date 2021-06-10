2d605158cb8ef6b9c53040dd7811c3ec57a63513
Dikshit Gupta <dgupta@mt.iitr.ac.in>
2021-06-10 08:57:50 +0530

    fix: Added sorting files

--- Sort/sorting.cpp
+++ Sort/sorting.cpp
@@ -7,17 +7,83 @@ void print_arr(vector <int> v){
     }cout<<endl;
 }
 
-vector <int> Bubble_sort(vector <int> v){
+//---------------------------Bubble Sort-------------------------------------------------------------------------------//
+/* 
+If we go through the inner loop with no swapping we can terminate our Search
+Best Case is when the array is already sorted then we return after one pass.
+Worst Case if the input is in descending order. O(n^2)
+*/
+
+vector <int> Bubble_sort(vector <int> &v){
     int n=v.size();
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-1-i;j++){
-                if(v[j]>v[j+1]){
-                    swap(v[j],v[j+1]);
-                }
+    bool swapped=false;
+    do{ 
+        swapped=false;
+        for(int i=0;i<n-1;i++){
+            if(v[i]>v[i+1]){
+              swap(v[i],v[i+1]);
+              swapped=true; 
+            }
         }
+        n--;
     }
+    while(swapped==true);
+
+    // int n=v.size();
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n-1-i;j++){
+    //             if(v[j]>v[j+1]){
+    //                 swap(v[j],v[j+1]);
+    //             }
+    //     }
+    // }
     return v;
 }
+
+//---------------------------Bubble Sort-------------------------------------------------------------------------------//
+
+//---------------------------Selection Sort-------------------------------------------------------------------------------//
+void selectionSort(vector <int> &v){
+    int n=v.size();
+
+    int maxi;
+    for(int i=n-1;i>=1;i--){
+        maxi=i;
+        for(int j=0;j<i;j++){
+            if(v[j]>=v[maxi])
+                maxi=j;
+        }
+        swap(v[i],v[maxi]);
+    }
+}
+//---------------------------Selection Sort-------------------------------------------------------------------------------//
+
+//---------------------------Insertion Sort-------------------------------------------------------------------------------//
+/*
+Best Case: already sorted: v[j]>next will be false. so it will run o(n)
+Worst case v[j]>next will be true and we have to shift all elements i.e reverse array like 5 4 3 2 1
+*/
+
+void insertionSort(vector <int> &v){
+    int n=v.size();
+    for(int i=1;i<n;i++){
+        int next=v[i];
+        int j;
+        for(j=i-1;j>=0 && v[j]>next;j--)
+            v[j+1]=v[j];    
+        
+        v[j+1]=next;
+    }
+}
+
+//---------------------------Insertion Sort--------------------------------------------------------------------------------//
+
+
+//---------------------------Merge Sort--------------------------------------------------------------------------------//
+/*
+ O(n lg(n))
+ o(n) extra space required for merge function in call stack.
+*/
 
 void merger_function (vector <int> & v,int l,int m,int r){
 int k=0;
@@ -46,11 +112,9 @@ for(int t=l ; t<=r; t++){
         k++;        
     }
 }
-
     for(int i=0;i<r-l+1;i++){
         v[l+i] = temp[i]; 
     }
-
 }
 
 void mergesort(vector <int> &v, int l, int r){
@@ -60,19 +124,27 @@ void mergesort(vector <int> &v, int l, int r){
     mergesort(v,m+1,r);
     merger_function(v,l,m,r);
 }
+//---------------------------Merge Sort----------------------------------------------------------------------------//
+
+//---------------------------Merge Sort----------------------------------------------------------------------------//
+
+
 
 int main() 
 {
    vector <int> v;
-   for(int i=0;i<60;i++) {
-       v.push_back(rand()%20);
+   for(int i=0;i<3;i++) {
+       v.push_back(rand()%1000);
    }
-    cout<<"orignial: ";
-    // mergesort(v,0,v.size()-1);
-    v=Bubble_sort(v);
     print_arr(v);
-    cout<<"sorted the array";
+    // mergesort(v,0,v.size()-1);
+    // Bubble_sort(v);
+    selectionSort(v);
+    // insertionSort(v);
+
+    print_arr(v);
     return 0;
+    
 }
 
 
